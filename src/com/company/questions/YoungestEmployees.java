3bdf48ea513cc6ac149413d046df7515411e17a4
Matthew Greco <mgreco2112@tutanota.com>
2022-08-31 17:55:12 -0400

    finished YoungestEmployees

---
+++ src/com/company/questions/YoungestEmployees.java
@@ -0,0 +1,10 @@
+package com.company.questions;
+
+public class YoungestEmployees {
+    //SELECT u.uin, e.name
+    //FROM employee_uin AS u
+    //RIGHT JOIN employee AS e
+    //ON u.id = e.id
+    //WHERE e.age < 25
+    //ORDER BY e.name ASC;
+}
