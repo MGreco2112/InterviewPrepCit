b25d8d655973239faf9c3c7eabf27b233600f147
Matthew Greco <mgreco2112@tutanota.com>
2022-08-31 19:46:06 -0400

    working on ValidEmailAddress regex

---
+++ src/com/company/questions/ValidEmailAddresses.java
@@ -0,0 +1,21 @@
+package com.company.questions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ValidEmailAddresses {
+    public static List<String> addresses = Arrays.asList(
+            "julia@hackerrank.com",
+            "julia_@hackerrank.com",
+            "julia_0@hackerrank.com",
+            "julia0_@hackerrank.com",
+            "julia@gmail.com"
+        );
+
+    public static void main(String[] args) {
+        for (String address : addresses) {
+            System.out.println(address.matches("[a-z]{1,6}_?[0-9]{0,4}(.*)@hackerrank.com"));
+        }
+    }
+}
