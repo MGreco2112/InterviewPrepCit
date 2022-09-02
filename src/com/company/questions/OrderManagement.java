4c8abc447b1f58feb1e53f5514ce4d3df9e84d7d
Matthew Greco <mgreco2112@tutanota.com>
2022-09-02 09:57:40 -0400

    finished OrderManagement

--- src/com/company/questions/OrderManagement.java
+++ src/com/company/questions/OrderManagement.java
@@ -1,24 +1,6 @@
 package com.company.questions;
 
 public class OrderManagement {
-//-- SELECT id, name
-//-- FROM customer_credit
-//-- INNER JOIN
-//-- 	(SELECT id, name FROM customer_credit GROUP BY name HAVING COUNT(name) > 1 ORDER BY id ASC)
-//--     TEMP ON customer.name = TEMP.name
-//-- ORDER BY name DESC;
-
-    //Attempt 2
-//    SELECT id, name from customer_credit ORDER BY
-//	(CASE
-//	WHEN COUNT(name) > 1 THEN name
-//    ELSE id
-//    END);
-
-    //Third and Closest Attempt Yet
-//    SELECT id, name FROM customer_credit GROUP BY name ORDER BY CASE
-//	WHEN COUNT(name) > 1 THEN id
-//    ELSE name
-//    END
-//    ;
+//SELECT id, name FROM customer_credit ORDER BY name DESC, id ASC;
 }
+//REMINDER!!! ORDER BY clauses can hold multiple conditionals
