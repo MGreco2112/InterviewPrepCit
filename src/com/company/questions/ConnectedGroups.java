f37424f858be647ac1eeba6bc2e29fb508e8f266
Matthew Greco <mgreco2112@tutanota.com>
2022-09-07 17:04:38 -0400

    playing with Matrix setup, attempting to isolate how to programatically tell when a relationship is not connected

--- src/com/company/questions/ConnectedGroups.java
+++ src/com/company/questions/ConnectedGroups.java
@@ -30,7 +30,39 @@ public class ConnectedGroups {
 //        return Collections.max(formattedOutput) - 1;
 
         //second attempt
-        //
+        //goal: identify how many groups there are based on connections
+        //create second matrix going down the rows for each person's relationships
+        //compare indexes of new matrix to old matrix
+
+        System.out.println("Input Matrix");
+
+        for (int i = 0; i < input.size(); i++) {
+            System.out.println(input.get(i));
+        }
+
+        List<List<Integer>> relationshipMatrix = new ArrayList<>();
+
+        System.out.println("Relationship Matrix");
+
+        int innerIndex = 0;
+
+        List<Integer> innerList = new ArrayList<>();
+
+        for (int i = 0; i <= input.size(); i++) {
+
+            if (i >= input.size()) {
+                innerIndex++;
+                i = 0;
+
+                System.out.println(innerList);
+                relationshipMatrix.add(innerList);
+                innerList.clear();
+            } else if (innerIndex == input.size()) {
+                break;
+            }
+
+            innerList.add(input.get(i).get(innerIndex));
+        }
 
         return 0;
 
@@ -48,7 +80,7 @@ public class ConnectedGroups {
         outerList.add(listC);
         outerList.add(listD);
 
-        System.out.println(solution(outerList));
+        solution(outerList);
     }
 }
 
