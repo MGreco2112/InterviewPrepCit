d0e009a8661f28cbbbe48833d7b7203a401e318b
Matthew Greco <mgreco2112@tutanota.com>
2022-09-06 20:11:38 -0400

    planning out solution 2

--- src/com/company/questions/ConnectedGroups.java
+++ src/com/company/questions/ConnectedGroups.java
@@ -9,23 +9,31 @@ public class ConnectedGroups {
     public static int solution(List<List<Integer>> input) {
         //This method creates an array with each index referencing one person
         //each index holds a number which says how many other people have a connection with that person
-        int[] output = new int[input.size()];
 
-        for (int i = 0; i < input.size(); i++) {
-            for (int j = 0; j < input.size(); j++) {
-                if (input.get(i).get(j) == 1) {
-                    output[i]++;
-                }
-            }
-        }
+        //first attempt
+//        int[] output = new int[input.size()];
+//
+//        for (int i = 0; i < input.size(); i++) {
+//            for (int j = 0; j < input.size(); j++) {
+//                if (input.get(i).get(j) == 1) {
+//                    output[i]++;
+//                }
+//            }
+//        }
+//
+//        List<Integer> formattedOutput = new ArrayList<>();
+//
+//        for (int connection : output) {
+//            formattedOutput.add(connection);
+//        }
+//
+//        return Collections.max(formattedOutput) - 1;
 
-        List<Integer> formattedOutput = new ArrayList<>();
+        //second attempt
+        //
 
-        for (int connection : output) {
-            formattedOutput.add(connection);
-        }
+        return 0;
 
-        return Collections.max(formattedOutput) - 1;
     }
 
     public static void main(String[] args) {
