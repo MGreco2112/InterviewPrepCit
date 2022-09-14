4c9cae13b04302d548614580833a4cb8204dc1df
Matthew Greco <mgreco2112@tutanota.com>
2022-09-13 20:17:47 -0400

    wrote out query from memory

---
+++ src/com/company/questions/MissedAssignmentSQL_Query.java
@@ -0,0 +1,17 @@
+package com.company.questions;
+
+public class MissedAssignmentSQL_Query {
+    /*
+        SELECT name FROM student
+        INNER JOIN
+        late_work
+        ON
+        student.name = late_work.student_id
+        ORDER BY name ASC;
+    */
+
+    //this resulted in duplicate entries of name and ID
+    //This wanted one student listed per id per missing work
+    //By using SELECT name, DISTINCT(id) at the top of the query got the correct answer, however the return format
+        //requested simply the name returned.
+}
